6f87410fe8290ba445d05eafa05c02de5ca9ec88
PHILIP B. STARK <pbstark@berkeley.edu>
2022-05-16 13:49:36 -0700

    WIP: stratum selector, multinomial

--- Code/utils.py
+++ Code/utils.py
@@ -1,3 +1,8 @@
+import numpy as np
+import scipy as sp
+from scipy.stats import bernoulli, multinomial
+from scipy.stats.mstats import gmean
+
 def alpha_mart(x: np.array, N: int, mu: float=1/2, eta: float=1-np.finfo(float).eps, u: float=1, \
                estim: callable=shrink_trunc) -> np.array :
     '''
@@ -118,6 +123,74 @@ def shrink_trunc(x: np.array, N: int, mu: float=1/2, nu: float=1-np.finfo(float)
     m = (N*mu-S)/(N-j+1) if np.isfinite(N) else mu   # mean of population after (j-1)st draw, if null is true 
     return np.minimum(u*(1-np.finfo(float).eps), np.maximum((d*nu+S)/(d+j-1),m+c/np.sqrt(d+j-1)))
 
+            
+def stratum_selector(marts : list, rule : callable, seed=None) -> np.array:
+    '''
+    select the order of strata from which the samples will be drawn to construct the test SM
+    
+    Parameters
+    ----------
+    marts: list of np.arrays
+        each list is the test supermartingale for one stratum
+    
+    rule: callable
+        maps three K-vectors (where K is the number of strata) to a value in {0, \ldots, K-1}, the stratum
+        from which the next term will be included in the product SM.
+        The rule should stop sampling from a stratum when that stratum is exhausted.
+        The first K-vector is the current value of each stratumwise SM
+        The second K-vector is the number of samples drawn from each stratum so far
+        The third is the number of elements in each stratum, or np.inf for sampling with replacement
+        
+    Returns
+    -------
+    strata : np.array
+        the series of strata from which the samples are included
+    T : np.array
+        the resulting product test SM
+    '''
+    strata = np.array([])
+    T = np.array([1])
+    running_T = np.ones(len(marts))  # current value of each stratumwise SM
+    running_n = np.zeros(len(marts)) # current index of each stratumwise SM
+    ns = np.zeros(len(marts))        # assumes the martingales exhaust the strata, for testing
+    for i in range(len(marts)):
+        ns[i] = len(marts[i])
+    t = 0
+    while any(running_n < ns-1):
+        t += 1
+        next_s = rule(running_T, running_n, ns, prng)
+        strata.append(next_s)
+        running_n[next_s] += 1
+        running_T[next_s] = marts[next_s][running_n[next_s]]
+        T[t] = np.prod(running_T)
+    return strata, T
+        
+
+def multinomial_selector(running_T : np.array, running_n : np.array, ns : np.array, prng : np.RandomState=None) -> int:
+    '''
+    find the next stratum from which to take a term for the product supermartingale test
+    
+    Parameters
+    ----------
+    running_t : np.array
+        the current value of each stratumwise SM
+    running_n : np.array
+        the number of samples drawn from each stratum so far
+    ns : np.array
+        the total number of items in each stratum, or np.inf for sampling with replacement
+    prng : np.RandomState
+        a PRNG (or seed, or none)   
+    '''
+    available = (running_n < ns-1)  # strata that aren't exhausted
+    if np.sum(available) == 0:
+        raise ValueError f'all strata are exhausted: {running_n=} {ns=}'
+    geomean = gmean(running_T[available])
+    ratios = running_T/geomean
+    return multinomial.rvs(1, ratios/np.sum(ratios), random_state=prng)
+    
+    
+##############################################################################
+
 def test_shrink_trunc():
     epsj = lambda c, d, j: c/math.sqrt(d+j-1)
     Sj = lambda x, j: 0 if j==1 else np.sum(x[0:j-1])
@@ -147,3 +220,11 @@ def test_shrink_trunc():
                 yfin[j-1] = np.minimum(np.maximum(muj(N,mu,x,j)+epsj(c,d,j), est), most)
             np.testing.assert_allclose(xinf, yinf)    
             np.testing.assert_allclose(xfin, yfin)    
+
+def test_multinomial_selector()
+    running_T = np.ones(3)
+    running_n = np.zeros(3)
+    ns = 
+    
+if __name__ == "__main__":
+    test_shrink_trunc()
