4d31fbbaabcda05a74d42bdbe446c4d33dd912c3
spertus <jakespertus@gmail.com>
2023-06-27 05:58:03 -0700

    modified comparison audit simulations and some utils; passing tests

--- Code/comparison_audits.py
+++ Code/comparison_audits.py
@@ -10,61 +10,54 @@ from utils import Bets, Allocations, Weights, mart, lower_confidence_bound, glob
     construct_eta_grid_plurcomp, simulate_comparison_audit
 
 
-N = [500, 500]
+N = [200, 200]
 
 grand_means = [0.5, 0.51, 0.53, 0.55, 0.57, 0.6, 0.65, 0.7, 0.75]
 stratum_gaps = [0.0, 0.1, 0.5]
 
 bets_dict = {"fixed":Bets.fixed, "agrapa":Bets.agrapa, "smooth_predictable":Bets.smooth_predictable}
-bets_list = ["fixed", "agrapa", "smooth_predictable", "uniform_mixture"]
+bets_list = ["fixed", "agrapa", "smooth_predictable"]
 allocations_dict = {"round_robin":Allocations.round_robin, "larger_means":Allocations.more_to_larger_means, "proportional_to_mart":Allocations.proportional_to_mart}
 allocations_list = ["round_robin", "larger_means", "proportional_to_mart"]
+methods_list = ['lcbs', 'ui-nnsm']
 
 
 results = []
-for grand_mean, gap, bet, allocation in itertools.product(grand_means, stratum_gaps, bets_list, allocations_list):
+for grand_mean, gap, method, bet, allocation in itertools.product(grand_means, stratum_gaps, methods_list, bets_list, allocations_list):
     A_c = [grand_mean - 0.5*gap, grand_mean + 0.5*gap]
+    #error rates are "0"
     p_1 = [0.0, 0.0]
     p_2 = [0.0, 0.0]
-    if bet == "uniform_mixture": #lcb doesn't do intersection mixtures
-        stopping_time_uinnsm = simulate_comparison_audit(
-            N, A_c, p_1, p_2,
-            lam_func = None,
-            allocation_func = allocations_dict[allocation],
-            method = "ui-nnsm",
-            combine = "product",
-            mixture = "uniform",
-            reps = 1,
-            WOR = True)[0]
-        stopping_time_lcb = None
-    else:
-        stopping_time_uinnsm = simulate_comparison_audit(
+    reps = 1 if allocation == "round_robin" else 30
+    if method == "lcbs":
+        if allocation == "proportional_to_mart":
+            stopping_time = None
+        else:
+            stopping_time = simulate_comparison_audit(
+                N, A_c, p_1, p_2,
+                lam_func = bets_dict[bet],
+                allocation_func = allocations_dict[allocation],
+                method = "lcbs",
+                reps = reps,
+                WOR = True)[0]
+    elif method == "ui-nnsm":
+        stopping_time = simulate_comparison_audit(
             N, A_c, p_1, p_2,
             lam_func = bets_dict[bet],
             allocation_func = allocations_dict[allocation],
             method = "ui-nnsm",
             combine = "product",
-            reps = 1,
+            reps = reps,
             WOR = True)[0]
-        if allocation == "proportional_to_mart": #lcb doesn't do eta adaptive allocation
-            stopping_time_lcb = None
-        else:
-            stopping_time_lcb = simulate_comparison_audit(
-                N, A_c, p_1, p_2,
-                lam_func = bets_dict[bet],
-                allocation_func = allocations_dict[allocation],
-                method = "lcbs",
-                reps = 1,
-                WOR = True)[0]
     data_dict = {
         "A_c":grand_mean,
         "stratum_gap":gap,
         "A_c_1":A_c[0],
         "A_c_2":A_c[1],
+        "method":str(method),
         "bet":str(bet),
         "allocation":str(allocation),
-        "stopping_time_uinnsm":stopping_time_uinnsm,
-        "stopping_time_lcb":stopping_time_lcb
+        "stopping_time":stopping_time
     }
     results.append(data_dict)
 results = pd.DataFrame(results)
